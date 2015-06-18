ccbabb15d739438c98452056048d2c6bb2c975e6
Jacob Swartzentruber <jsswart@gmail.com>
2015-06-18 18:34:18 -0500

    Terrain is now procedurally generated.  Side scrolling for player and terrain implemented.  Background has parallax scrolling with three different scenes.

--- scripts.js
+++ scripts.js
@@ -23,53 +23,24 @@ var canvas = document.getElementById("canvas"),
 		keys = [],
 		friction = 0.8,
 		gravity = 0.3;
- 
+
 canvas.width = width;
 canvas.height = height;
 
-//Adding obstacles to our level
+//Adding background images to the level
+var background = [];
+background.push({x: 0, y: 0, img: new Image()},{x: 0, y: 0, img: new Image()},{x: 0, y: 0, img: new Image()});
+background[0].img.src = 'assets/sky.png';
+background[1].img.src = 'assets/mountains.png';
+background[2].img.src = 'assets/treeline.png';
+
+//Adding obstacles to the level
 var boxes = [];
-boxes.push({
-    x: 0,
-    y: 0,
-    width: 10,
-    height: height
-});
 boxes.push({
     x: 0,
     y: height - 2,
     width: width,
     height: 50
-});
-boxes.push({
-    x: width - 10,
-    y: 0,
-    width: 50,
-    height: height
-});
-boxes.push({
-    x: 120,
-    y: 10,
-    width: 80,
-    height: 80
-});
-boxes.push({
-    x: 170,
-    y: 50,
-    width: 80,
-    height: 80
-});
-boxes.push({
-    x: 220,
-    y: 100,
-    width: 80,
-    height: 80
-});
-boxes.push({
-    x: 270,
-    y: 150,
-    width: 40,
-    height: 40
 });
  
 // The update function which calculates each animation frame
@@ -96,6 +67,40 @@ function update(){
 		}
 	}
 
+	//Add box to end of level if needed
+	var last_index = boxes.length-1;
+	if(boxes[last_index].x + boxes[last_index].width <= width+2){
+		var tempHeight = 2+Math.random()*(boxes[last_index].height-20);
+		if(tempHeight > height+30){
+			tempHeight = height+30;
+		}
+		boxes.push({
+			x: width,
+	    y: height - Math.floor(tempHeight),
+	    width: 10+Math.floor(Math.random()*80),
+	    height: 50+tempHeight
+		});
+	}
+
+	//Adjust background, player and box positions for scrolling effect
+	//Background
+	for(var i=0; i<background.length; i++){
+		if(background[i].x <= -width*2){
+			background[i].x = 0;
+		}
+		background[i].x -= 0.2*(i+1);
+	}
+	//Player
+	player.x -= 1;
+	//Box
+	if(boxes[0].x+boxes[0].width < 0){
+		boxes.splice(0,1);
+	}
+	for(var i=0; i<boxes.length; i++){
+		boxes[i].x -= 1;
+	}
+
+
 	//Adjust player velocity in accordance with gravity and friction
 	player.velX *= friction;
 	player.velY += gravity;
@@ -103,8 +108,14 @@ function update(){
   //Clear previous animation frame from canvas
   ctx.clearRect(0,0,width,height);
 
+  //Draw the background
+  for(var i=0; i<background.length; i++){
+  	ctx.drawImage(background[i].img, background[i].x, background[i].y);
+  	ctx.drawImage(background[i].img, background[i].x+width*2, background[i].y);
+  }
+
   //Draw our obstacles
-  ctx.fillStyle = "black";
+  ctx.fillStyle = "grey";
 	ctx.beginPath();
 	player.grounded = false;
 	for (var i = 0; i < boxes.length; i++) {
@@ -128,6 +139,9 @@ function update(){
 	player.x += player.velX;
 	player.y += player.velY;
 
+	ctx.fillText("Player X:"+player.x+" Y:"+player.y,10,10);
+	ctx.fillText("Jumping:"+player.jumping+" Grounded:"+player.grounded,10,30);
+
 	//Draw player
   ctx.fillStyle = "red";
   ctx.fillRect(player.x, player.y, player.width, player.height);
@@ -149,7 +163,7 @@ function colCheck(shapeA, shapeB) {
   if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
   	// figures out on which side we are colliding (top, bottom, left, or right)
   	var oX = hWidths - Math.abs(vX),
-  			oY = hHeights - Math.abs(vY);
+  			oY = (hHeights - Math.abs(vY)).toFixed(8);
 		if (oX >= oY) {
 		  if (vY > 0) {
 		    colDir = "t";
